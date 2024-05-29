041f90e2ab086581533a3bb9ac8198df838a7bd7
Tarek Telleria <tarekizar.telleria@students.salle.url.edu>
2024-05-29 14:09:36 +0200

    Initial commit without large file

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var dropdowns = document.querySelectorAll(".dropdown");
+
+    dropdowns.forEach(function(dropdown) {
+        dropdown.addEventListener("click", function() {
+            var content = this.parentElement.nextElementSibling;
+            if (content.classList.contains("show")) {
+                content.classList.remove("show");
+            } else {
+                content.classList.add("show");
+            }
+        });
+    });
+});
+
+
+    document.addEventListener("DOMContentLoaded", function() {
+        var mediaContainer = document.querySelector(".media-container");
+        var video = mediaContainer.querySelector("video");
+
+        mediaContainer.addEventListener("mouseover", function() {
+            video.play();
+        });
+
+        mediaContainer.addEventListener("mouseout", function() {
+            video.pause();
+            video.currentTime = 0; // Reset video to the start when hover ends
+        });
+    });
